fc9663c534e969c11f6b70d75c67ca557b4ff998
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-10-16 19:28:47 -0300

    Add get handler

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>');
+app.use(express.json());
+
+const corsOptions = {
+  origin: true,
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
+app.get('/', async (req, res) => {
+  try {
+    res.status(200).json({ message: "hello world" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-app.listen(3000);
+app.listen(4000);
